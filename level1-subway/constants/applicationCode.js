1d619225b7684918ac948d8c524cb7291101f654
sunhpark42 <sunhpark42@gmail.com>
2021-10-24 10:25:47 +0900

    fix: change the way to get a token in auth middleware

---
+++ level1-subway/constants/applicationCode.js
@@ -0,0 +1,12 @@
+const APPLICATION_CODE = {
+  // common
+  BAD_REQUEST: 400,
+
+  // authentification
+  FIND_USER_BY_TOKEN_ERROR: 1000,
+
+  // stations : 2000 - 2999
+  CREATE_STATION_ERROR: 2000,
+};
+
+module.exports = APPLICATION_CODE;
--- middleware/auth.js
+++ middleware/auth.js
@@ -1,19 +1,24 @@
+const APPLICATION_CODE = require('../level1-subway/constants/applicationCode');
 const { User } = require('../level1-subway/models/User');
 
 const auth = (req, res, next) => {
-  // 인증처리를 하는 부분
-  // 클라이언트 쿠키에서 토큰 가져오기
-  const token = req.cookies.x_auth;
+  const token = req.headers.authorization?.split(' ')[1];
+
+  if (!token) {
+    return res.status(400).json({ code: APPLICATION_CODE.BAD_REQUEST, message: '토큰 정보가 없습니다.' });
+  }
 
   // 토큰을 복호화 한 후 유저를 찾는다.
-
   User.findByToken(token, (error, user) => {
     if (error) {
       throw error;
     }
 
     if (!user) {
-      return res.json({ isAuthenticated: false, error: true });
+      return res.status(400).json({
+        code: APPLICATION_CODE.FIND_USER_BY_TOKEN_ERROR,
+        message: '해당 토큰을 사용하는 유저를 찾을 수 없습니다.',
+      });
     }
 
     // 이후 cb에서 정보를 이용하기 위함
