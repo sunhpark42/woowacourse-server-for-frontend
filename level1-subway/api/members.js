bb9dcbc8c47bc97a5eccedf6c8a56a6cd0bb7ce0
sunhpark42 <sunhpark42@gmail.com>
2021-10-27 13:16:02 +0900

    fix: import error

--- level1-subway/api/members.js
+++ level1-subway/api/members.js
@@ -28,12 +28,12 @@ memberRouter.post(`/`, (req, res) => {
 // 이메일 중복체크
 memberRouter.get(`/check-validation`, (req, res) => {
   if (!req.query.email) {
-    return res.status(400).json({ code: APPLICATION_CODE.BAD_REQUEST, message: '쿼리 파라미터가 유효하지 않습니다.' });
+    return res.status(400).json({ code: APPLICATION_CODE?.BAD_REQUEST, message: '쿼리 파라미터가 유효하지 않습니다.' });
   }
 
   User.findOne({ email: req.query.email }, (error, user) => {
     if (user) {
-      return res.status(400).json({ code: APPLICATION_CODE.EMAIL_DUPLICATED_ERROR, message: '중복된 이메일 입니다.' });
+      return res.status(400).json({ code: APPLICATION_CODE?.EMAIL_DUPLICATED_ERROR, message: '중복된 이메일 입니다.' });
     } else {
       return res.status(200).json({ message: '중복된 이메일이 아닙니다.' });
     }
